5ff4ea760f49be970a4f189353babc7c51f82aeb
sparksandpatterns <kira.schacht@gmail.com>
2016-04-30 13:02:46 +0200

    Add break option to tile plot

--- tot.heat.sub.R
+++ tot.heat.sub.R
@@ -5,8 +5,9 @@ load("LDA-Sozialismus/Sozlda-k10i20b70s24601.Rdata")
 load("Spiegel/Spiegel-meta.Rdata")
 
 tot.heat.sub(x = result, ldaID = ldaID, meta = meta,
-             file = "LDA-Sozialismus/tot-heat.pdf",
-             Tnames = row.names(mtcars)[1:10])
+             file = "LDA-Sozialismus/tot-heat-5.pdf",
+             Tnames = row.names(mtcars)[1:10],
+             date_breaks = "5 years")
 
 #tot.heat.sub returns a pdf document with a heat map showing the deviations
 #of each topic from its mean share in all topics,
@@ -17,9 +18,9 @@ tot.heat.sub(x = result, ldaID = ldaID, meta = meta,
 # meta: The meta data for the texts.
 # file: Name of the pdf file.
 # Tnames: Label for the topics
-# smooth: How much the output should be smoothed. Set to 0 for no smoothing.
+# date_breaks: Which years should be shown on the x axis. Either "1 year","5 years" or "10 years".
 
-tot.heat.sub <- function(x, ldaID, meta, file, Tnames = 1:10, ...){
+tot.heat.sub <- function(x, ldaID, meta, file, Tnames = 1:10, date_breaks = "1 year", ...){
 
 #pakete laden
 require("reshape2")
@@ -75,7 +76,7 @@ theme_none <- theme(
       panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(),
       panel.background = element_blank(),
-      axis.title.x = element_text(colour=NA),
+      axis.title.x = element_blank(),
       axis.title.y = element_blank(),
       axis.text.x = element_blank(),
       axis.text.y = element_blank(),
@@ -83,8 +84,14 @@ theme_none <- theme(
       axis.ticks = element_blank()
 )
 
+#breaks bestimmen
+if(date_breaks =="1 year")   breaks <- unique(tmpdate)
+if(date_breaks =="5 years")  breaks <- as.Date(paste0(unique(5*round(year(tmpdate)/5)), "-01-01"))
+if(date_breaks =="10 years") breaks <- as.Date(paste0(unique(10*round(year(tmpdate)/10)), "-01-01"))
+
 #plotten
 (cat("Plotten..\n"))
+
 pdf(file, width = 14)
 
 #Tile plot
@@ -94,12 +101,11 @@ p1 <- ggplot(tmp, aes(date, topic)) +
                            high = "#ca0020", midpoint = 0) +
       theme_light(base_size = 20) +
       labs(x = "", y = "") +
-      scale_x_date(expand = c(0, 0), date_breaks = "1 year", date_labels = "%Y") +
+      scale_x_date(expand = c(0, 0), breaks = breaks, date_labels = "%Y") +
       scale_y_discrete(expand = c(0, 0)) +
       guides(fill=guide_colourbar(title=NULL)) +
-      theme(axis.ticks = element_blank(),
-            axis.text.x = element_text(size = 16,
-                                       angle = 300, hjust = 0),
+      theme(axis.ticks.y = element_blank(),
+            axis.text.x = element_text(size = 13, hjust = 0, angle = 300),
             axis.text.y = element_text(size = 16)
       ) +
       coord_fixed(ratio = 400)
@@ -107,8 +113,7 @@ p1 <- ggplot(tmp, aes(date, topic)) +
 # Dendrogram
 p2 <- ggplot(segment(ddata)) + 
       geom_segment(aes(x=x, y=y, xend=xend, yend=yend)) + 
-      theme_none + theme(axis.title.x=element_blank(),
-                         plot.margin = unit(c(-0.1,-1,-0.1,0),"cm")) +
+      theme_none + theme(plot.margin = unit(c(-0.1,-1,-0.1,0),"cm")) +
       coord_flip() + scale_y_reverse()
 
 #Plots in einem gtable zusammenfügen
@@ -117,7 +122,6 @@ g <- ggplotGrob(p1) %>%
       gtable_add_cols(unit(3,"cm"), pos = 0) %>%
       gtable_add_grob(g2, t = 2, l=1, b=3, r=1)
 grid.draw(g)
-
 #Überschrift
 grid.text("Abweichung der Topics von ihrem Durchschnittsanteil", x=0.5,y=0.9,
           just = c("center", "top"), gp = gpar(fontsize = 20))
